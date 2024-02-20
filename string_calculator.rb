170fd67c24f2078c9af0a981de7768faa0d8ee8b
Shubham Taywade <shubhamtaywade82@gmail.com>
2024-02-20 22:13:36 +0530

    Implement basic functionality for handling empty string.

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,11 @@
 
 # Performs calculations on strings containing numbers.
 class StringCalculator
-
+  # Adds numbers from a string, returning the sum.
+  #
+  # @param input_string [String] The string containing numbers to be added.
+  # @return [Integer] The sum of the numbers in the string.
+  def add(input_string)
+    0 if input_string.empty?
+  end
 end
