c76cd3e2f4df36b2b0de215871d818bcb025a7f7
Shubham Taywade <shubhamtaywade82@gmail.com>
2024-02-21 18:39:27 +0530

    Refactor code for improved readability

--- string_calculator.rb
+++ string_calculator.rb
@@ -9,19 +9,36 @@ class StringCalculator
   def add(input_string)
     return 0 if input_string.nil? || input_string.empty?
 
-    delimiter = ','
-    numbers = input_string
+    delimiter, numbers = extract_delimiter_and_numbers(input_string)
+    numbers = numbers.split(/[\n#{delimiter}]/).map(&:to_i)
+    handle_negatives(numbers)
 
+    numbers.sum
+  end
+
+  private
+
+  # Extracts the delimiter and numbers from the input string.
+  #
+  # @param input_string [String] The input string containing numbers.
+  # @return [Array(String, String)] The delimiter and numbers extracted from the input string.
+  def extract_delimiter_and_numbers(input_string)
     if input_string.start_with?('//')
       delimiter = input_string[2]
       numbers = input_string[4..]
+    else
+      delimiter = ','
+      numbers = input_string
     end
+    [delimiter, numbers]
+  end
 
-    numbers = numbers.split(/[\n#{delimiter}]/).map(&:to_i)
+  # Raises an exception if any negative numbers are present in the input array.
+  #
+  # @param numbers [Array<Integer>] The array of numbers to be checked for negativity.
+  # @raise [RuntimeError] If negative numbers are present in the input array.
+  def handle_negatives(numbers)
     negatives = numbers.select(&:negative?)
-
     raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
-
-    numbers.sum
   end
 end
